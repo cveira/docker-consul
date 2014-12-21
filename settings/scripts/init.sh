604df41b9f26da61daca923eec3e8fd92896aa61
Carlos Veira Lorenzo <cveira@gmail.com>
2014-12-21 18:55:45 +0100

    0.1a

--- settings/scripts/init.sh
+++ settings/scripts/init.sh
@@ -15,13 +15,13 @@ case "$( echo '$@' | tr '-' '\n' | grep 'join' | wc -l )" in
 		export META_CONSUL_MASTER=$( nmap -p53 ${META_NET_MASTERSEGMENT}/24 | \
 																 grep -B 3 "53/tcp open  domain" | head -1 | tr " " "\n" | tail -1 )
 
-		sed -i "s/{{META_SERVER_IP}}/${META_SERVER_IP}/g"         /etc/supervisor/conf.d/supervisord-secondary.conf
-		sed -i "s/{{META_CONSUL_MASTER}}/${META_CONSUL_MASTER}/g" /etc/supervisor/conf.d/supervisord-secondary.conf
+		sed -i "s/{META_SERVER_IP}/${META_SERVER_IP}/g"         /etc/supervisor/conf.d/supervisord-secondary.conf
+		sed -i "s/{META_CONSUL_MASTER}/${META_CONSUL_MASTER}/g" /etc/supervisor/conf.d/supervisord-secondary.conf
 
 		/usr/local/bin/confd -onetime
 		/usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord-secondary.conf
 	*)
-		sed -i "s/{{META_SERVER_IP}}/${META_SERVER_IP}/g"         /etc/supervisor/conf.d/supervisord-primary.conf
+		sed -i "s/{META_SERVER_IP}/${META_SERVER_IP}/g"         /etc/supervisor/conf.d/supervisord-primary.conf
 
 		/usr/local/bin/confd -onetime
 		/usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord-primary.conf
