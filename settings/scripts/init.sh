4ff7dd1d1051e03c7bcf1c1d06a37fbb67babfaf
Carlos Veira Lorenzo <cveira@gmail.com>
2014-12-21 19:50:08 +0100

    0.1a

--- settings/scripts/init.sh
+++ settings/scripts/init.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 if [ $META_NET_REMOTESEGMENTS ]; then
-	for IPNetwork in $( echo $META_NET_REMOTESEGMENTS | tr ";" "\n" ); do
-	  route add -net $IPNetwork netmask $META_NET_MASK gw $META_NET_GATEWAY
-	done
+  for IPNetwork in $( echo $META_NET_REMOTESEGMENTS | tr ";" "\n" ); do
+    route add -net $IPNetwork netmask $META_NET_MASK gw $META_NET_GATEWAY
+  done
 fi
 
 
@@ -11,18 +11,20 @@ export META_SERVER_IP="$(ifconfig eth0 | awk -F ' *|:' '/inet addr/{print $4}')"
 export META_CONSUL_PARAMS="$@"
 
 case "$( echo '$@' | tr '-' '\n' | grep 'join' | wc -l )" in
-	"1")
-		export META_CONSUL_MASTER=$( nmap -p53 ${META_NET_MASTERSEGMENT}/24 | \
-																 grep -B 3 "53/tcp open  domain" | head -1 | tr " " "\n" | tail -1 )
+  "1")
+    export META_CONSUL_MASTER=$( nmap -p53 ${META_NET_MASTERSEGMENT}/24 | \
+                                 grep -B 3 "53/tcp open  domain" | head -1 | tr " " "\n" | tail -1 )
 
-		sed -i "s/{META_SERVER_IP}/${META_SERVER_IP}/g"         /etc/supervisor/conf.d/supervisord-secondary.conf
-		sed -i "s/{META_CONSUL_MASTER}/${META_CONSUL_MASTER}/g" /etc/supervisor/conf.d/supervisord-secondary.conf
+    sed -i "s/{META_SERVER_IP}/${META_SERVER_IP}/g"         /etc/supervisor/conf.d/supervisord-secondary.conf
+    sed -i "s/{META_CONSUL_MASTER}/${META_CONSUL_MASTER}/g" /etc/supervisor/conf.d/supervisord-secondary.conf
 
-		/usr/local/bin/confd -onetime
-		/usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord-secondary.conf
-	*)
-		sed -i "s/{META_SERVER_IP}/${META_SERVER_IP}/g"         /etc/supervisor/conf.d/supervisord-primary.conf
+    /usr/local/bin/confd -onetime
+    /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord-secondary.conf
+    ;;
+  *)
+    sed -i "s/{META_SERVER_IP}/${META_SERVER_IP}/g"         /etc/supervisor/conf.d/supervisord-primary.conf
 
-		/usr/local/bin/confd -onetime
-		/usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord-primary.conf
+    /usr/local/bin/confd -onetime
+    /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord-primary.conf
+    ;;
 esac
