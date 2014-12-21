a70ede12a056162d671677072591814ca44014cd
Carlos Veira Lorenzo <cveira@gmail.com>
2014-12-21 18:45:31 +0100

    0.1a

--- settings/scripts/init.sh
+++ settings/scripts/init.sh
@@ -6,5 +6,23 @@ if [ $META_NET_REMOTESEGMENTS ]; then
 	done
 fi
 
-/usr/local/bin/confd -onetime
-/usr/bin/supervisord
+
+export META_SERVER_IP="$(ifconfig eth0 | awk -F ' *|:' '/inet addr/{print $4}')"
+export META_CONSUL_PARAMS="$@"
+
+case "$( echo '$@' | tr '-' '\n' | grep 'join' | wc -l )" in
+	"1")
+		export META_CONSUL_MASTER=$( nmap -p53 ${META_NET_MASTERSEGMENT}/24 | \
+																 grep -B 3 "53/tcp open  domain" | head -1 | tr " " "\n" | tail -1 )
+
+		sed -i "s/{{META_SERVER_IP}}/${META_SERVER_IP}/g"         /etc/supervisor/conf.d/supervisord-secondary.conf
+		sed -i "s/{{META_CONSUL_MASTER}}/${META_CONSUL_MASTER}/g" /etc/supervisor/conf.d/supervisord-secondary.conf
+
+		/usr/local/bin/confd -onetime
+		/usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord-secondary.conf
+	*)
+		sed -i "s/{{META_SERVER_IP}}/${META_SERVER_IP}/g"         /etc/supervisor/conf.d/supervisord-primary.conf
+
+		/usr/local/bin/confd -onetime
+		/usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord-primary.conf
+esac
