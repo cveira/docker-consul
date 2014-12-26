00fc2ac6cac9755310995b9cd14a0687dafe7d0a
Carlos Veira Lorenzo <cveira@gmail.com>
2014-12-26 22:36:41 +0100

    0.1a

--- settings/scripts/init.sh
+++ settings/scripts/init.sh
@@ -7,28 +7,61 @@ if [ $META_NET_REMOTESEGMENTS ]; then
 fi
 
 
-META_NET_MASTERSEGMENT="$( echo ${META_NET_REMOTESEGMENTS} | tr ";" "\n" | head -1 )"
-META_SERVER_IP="$(ifconfig eth0 | awk -F ' *|:' '/inet addr/{print $4}')"
+export META_NET_MASTERSEGMENT="$( echo ${META_NET_REMOTESEGMENTS} | tr ";" "\n" | head -1 )"
+export META_NODE_IP="$(ifconfig eth0 | awk -F ' *|:' '/inet addr/{print $4}')"
 
 
-if [ "$1" == "-join" ]; then
-  export META_CONSUL_MASTER=$( nmap -p53 ${META_NET_MASTERSEGMENT}/24 | \
-                               grep -B 3 "53/tcp open  domain" | head -1 | tr " " "\n" | tail -1 )
+if [ ! -f /etc/confd/conf.d/supervisord.conf.toml ]; then
+  if [ "$1" == "-join" ]; then
+    touch /root/tmp/.joinConsulCluster
 
-  sed -i "s/{META_SERVER_IP}/${META_SERVER_IP}/g"         /etc/supervisor/conf.d/supervisord-secondary.conf
-  sed -i "s/{META_CONSUL_MASTER}/${META_CONSUL_MASTER}/g" /etc/supervisor/conf.d/supervisord-secondary.conf
+    export META_CONSUL_MASTERSERVERIP=$( nmap -p53 ${META_NET_MASTERSEGMENT}/24 | \
+                                         grep -B 3 "53/tcp open  domain" | head -1 | tr " " "\n" | tail -1 )
 
-  /usr/local/bin/confd -onetime
-  rm -f /etc/supervisor/conf.d/supervisord-primary.conf
-  mv /etc/supervisor/conf.d/supervisord-secondary.conf    /etc/supervisor/conf.d/supervisord.conf
+    rm -f /etc/confd/conf.d/supervisord-primary.conf.toml
+    rm -f /etc/confd/templates/supervisord-primary.conf.tmpl
 
-  /usr/bin/supervisord
-else
-  sed -i "s/{META_SERVER_IP}/${META_SERVER_IP}/g"         /etc/supervisor/conf.d/supervisord-primary.conf
+    mv /etc/confd/conf.d/supervisord-secondary.conf.toml    /etc/confd/conf.d/supervisord.conf.toml
+    mv /etc/confd/templates/supervisord-secondary.conf.tmpl /etc/confd/templates/supervisord.conf.tmpl
+  else
+    rm -f /etc/confd/conf.d/supervisord-secondary.conf.toml
+    rm -f /etc/confd/templates/supervisord-secondary.conf.tmpl
 
-  /usr/local/bin/confd -onetime
-  rm -f /etc/supervisor/conf.d/supervisord-secondary.conf
-  mv /etc/supervisor/conf.d/supervisord-primary.conf      /etc/supervisor/conf.d/supervisord.conf
-
-  /usr/bin/supervisord
+    mv /etc/confd/conf.d/supervisord-primary.conf.toml      /etc/confd/conf.d/supervisord.conf.toml
+    mv /etc/confd/templates/supervisord-primary.conf.tmpl   /etc/confd/templates/supervisord.conf.tmpl
+  fi
 fi
+
+
+if [ -f /root/tmp/.joinConsulCluster ]; then
+  export META_CONSUL_MASTERSERVERIP=$( nmap -p53 ${META_NET_MASTERSEGMENT}/24 | \
+                                       grep -B 3 "53/tcp open  domain" | head -1 | tr " " "\n" | tail -1 )
+fi
+
+
+if [ -f /var/run/rsyslogd.pid                       ]; then rm -f /var/run/rsyslogd.pid                       ; fi
+if [ -f /var/run/monit.pid                          ]; then rm -f /var/run/monit.pid                          ; fi
+if [ -f /var/run/newrelic-daemon.pid                ]; then rm -f /var/run/newrelic-daemon.pid                ; fi
+if [ -f /var/run/newrelic/newrelic-plugin-agent.pid ]; then rm -f /var/run/newrelic/newrelic-plugin-agent.pid ; fi
+if [ -f /var/run/sshd.pid                           ]; then rm -f /var/run/sshd.pid                           ; fi
+if [ -f /var/run/supervisord.pid                    ]; then rm -f /var/run/supervisord.pid                    ; fi
+
+if [ -f /var/spool/postfix/pid/master.pid           ]; then rm -f /var/spool/postfix/pid/master.pid           ; fi
+if [ -f /var/run/memcached.pid                      ]; then rm -f /var/run/memcached.pid                      ; fi
+if [ -f /var/run/mysqld/mysqld.pid                  ]; then rm -f /var/run/mysqld/mysqld.pid                  ; fi
+if [ -f /var/run/haproxy.pid                        ]; then rm -f /var/run/haproxy.pid                        ; fi
+if [ -f /var/run/lsyncd.pid                         ]; then rm -f /var/run/lsyncd.pid                         ; fi
+if [ -f /var/run/apache2/apache2.pid                ]; then rm -f /var/run/apache2/apache2.pid                ; fi
+
+
+echo
+echo "Boot time configuration dependencies:"
+echo "  + META_NODE_NAME:             $META_NODE_NAME"
+echo "  + META_NODE_IP:               $META_NODE_IP"
+echo "  + META_NET_MASTERSEGMENT:     $META_NET_MASTERSEGMENT"
+echo "  + META_CONSUL_MASTERSERVERIP: $META_CONSUL_MASTERSERVERIP"
+echo
+
+
+/usr/local/bin/confd -onetime
+/usr/bin/supervisord
